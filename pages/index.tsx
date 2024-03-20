c289f87f62a1675d75c6db4ce156dd7ae0c9b43f
yusufdgzl <yusuf.dg13@gmail.com>
2024-03-20 16:52:23 +0300

    completed main css design

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
       <div className="flex flex-col w-full bg-white md:flex-row max-w-[1200px] ">
         <div className="relative w-full">
           <img
-            className="hidden  md:flex max-h-[700px] "
+            className="hidden md:flex md:h-[700px] "
             src="/images/bg-main-desktop.png"
             alt=""
           />
@@ -13,16 +13,40 @@ export default function HomePage() {
             src="/images/bg-main-mobile.png "
             alt=""
           />
-          <img
-            className=" w-[340px] md:w-[400px] absolute top-10 right-6 md:top-auto md:-right-14 md:bottom-[110px] "
-            src="images/bg-card-back.png"
-            alt=""
-          />
-          <img
-            className=" w-[340px] md:w-[400px] absolute -bottom-10 left-6 md:left-auto md:bottom-auto md:top-32 md:right-7  "
-            src="images/bg-card-front.png"
-            alt=""
-          />
+          <div className="absolute top-10 right-6 md:top-auto md:-right-14 md:bottom-[110px] ">
+            <div className="relative ">
+              <img
+                className=" w-[340px] md:w-[400px] "
+                src="images/bg-card-back.png"
+                alt=""
+              />
+              <p className="absolute z-10 text-white tracking-wider right-10 top-20 md:top-24  ">
+                000
+              </p>
+            </div>
+          </div>
+          <div className="absolute -bottom-10 left-6 md:left-auto md:bottom-auto md:top-32 md:right-7  ">
+            <div className="flex relative">
+              <img
+                className=" w-[340px] md:w-[400px] "
+                src="images/bg-card-front.png"
+                alt=""
+              />
+              <div className="flex flex-col absolute  z-10 p-6 md:px-10 w-full h-full text-white justify-between ">
+                  <div className="flex items-center space-x-5">
+                    <div className="w-10 h-10 ring-1 rounded-full bg-white"></div>
+                    <div className="w-4 h-4 rounded-full ring-1 ring-yellow-500"></div>
+                  </div>
+                  <div className="flex flex-col space-y-2 ">
+                    <p className="font-serif text-2xl tracking-wider md:tracking-widest">0000 0000 0000 0000</p>
+                    <div className="flex justify-between">
+                      <p>Jane Appleseed</p>
+                      <p>00/00</p>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="flex flex-col w-full p-6 pt-20  font-semibold font-mono space-y-8 md:p-40  ">
@@ -84,7 +108,9 @@ export default function HomePage() {
               />
             </div>
           </div>
-          <button className="bg-sky-950 py-3 text-white rounded-xl text-md font-sans border-b-4 border-sky-800  hover:border-b-0 hover:border-t-4 hover:border-t-sky-950 shadow-md hover:shadow-sky-600 ">Confirm</button>
+          <button className="bg-sky-950 py-3 text-white rounded-xl text-md font-sans border-b-4 border-sky-800  hover:border-b-0 hover:border-t-4 hover:border-t-sky-950 shadow-md hover:shadow-sky-600 ">
+            Confirm
+          </button>
         </div>
       </div>
     </div>
